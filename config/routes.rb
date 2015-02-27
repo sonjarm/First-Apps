923c8aff7f678ba4f818428360ee728935b73e1d
Sonja R. Maddox <[sonjarmaddox@yahoo.com]>
2015-02-26 21:11:29 -0600

    initial commit for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
-
   resources :to_dos
 
   root to: 'todos#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212142238) do
+ActiveRecord::Schema.define(version: 20150209153601) do
 
   create_table "to_dos", force: true do |t|
     t.string   "title"
     t.text     "notes"
     t.date     "due"
     t.boolean  "done"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
